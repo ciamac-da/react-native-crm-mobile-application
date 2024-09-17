4c1699e947630e33a14fa29f9d16ea34880671c1
Ciamac.Davoudi <ciamac.davoudil@interone.de>
2024-09-17 13:41:42 +0200

    timers

--- components/Game/Game.tsx
+++ components/Game/Game.tsx
@@ -5,28 +5,101 @@ import PropTypes from "prop-types";
 
 interface GameProps {
   randomNumberCount: number;
+  initialSeconds: number;
 }
 
 interface GameState {
   selectedIds: number[];
+  remainingSeconds: number;
 }
 
 class Game extends React.Component<GameProps, GameState> {
+  intervalId: NodeJS.Timeout | null = null;
+
+  static defaultProps = {
+    initialSeconds: 10,
+  };
+
   static propTypes = {
     randomNumberCount: PropTypes.number.isRequired,
+    initialSeconds: PropTypes.number.isRequired,
   };
+
   state: GameState = {
     selectedIds: [],
+    remainingSeconds: this.props.initialSeconds || 10,
   };
-  randomNumbers = Array.from({
-    length: this.props.randomNumberCount,
-  }).map(() => 1 + Math.floor(10 * Math.random()));
 
-  target = this.randomNumbers
-    .slice(0, this.props.randomNumberCount - 2)
-    .reduce((acc, curr) => acc + curr, 0);
+  randomNumbers = this.generateRandomNumbers();
 
-  isNumberSlected = (numberIndex: number) => {
+  target = this.calculateTarget(this.randomNumbers);
+
+  generateRandomNumbers() {
+    return Array.from({
+      length: this.props.randomNumberCount,
+    }).map(() => 1 + Math.floor(10 * Math.random()));
+  }
+
+  calculateTarget(randomNumbers: number[]) {
+    return randomNumbers
+      .slice(0, this.props.randomNumberCount - 2)
+      .reduce((acc, curr) => acc + curr, 0);
+  }
+
+  resetGame = () => {
+    const randomNumbers = this.generateRandomNumbers();
+    const target = this.calculateTarget(randomNumbers);
+    this.setState({
+      selectedIds: [],
+      remainingSeconds: this.props.initialSeconds || 10,
+    });
+    this.randomNumbers = randomNumbers;
+    this.target = target;
+
+    if (this.intervalId !== null) {
+      clearInterval(this.intervalId);
+    }
+
+    this.intervalId = setInterval(() => {
+      this.setState(
+        (prevState) => {
+          return { remainingSeconds: prevState.remainingSeconds - 1 };
+        },
+        () => {
+          if (this.state.remainingSeconds === 0) {
+            if (this.intervalId !== null) {
+              clearInterval(this.intervalId);
+            }
+          }
+        }
+      );
+    }, 1000);
+  };
+
+  componentDidMount(): void {
+    this.intervalId = setInterval(() => {
+      this.setState(
+        (prevState) => {
+          return { remainingSeconds: prevState.remainingSeconds - 1 };
+        },
+        () => {
+          if (this.state.remainingSeconds === 0) {
+            if (this.intervalId !== null) {
+              clearInterval(this.intervalId);
+            }
+          }
+        }
+      );
+    }, 1000);
+  }
+
+  componentWillUnmount(): void {
+    if (this.intervalId !== null) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  isNumberSelected = (numberIndex: number) => {
     return this.state.selectedIds.indexOf(numberIndex) >= 0;
   };
 
@@ -40,14 +113,24 @@ class Game extends React.Component<GameProps, GameState> {
     const sumSelected = this.state.selectedIds.reduce((acc, curr) => {
       return acc + this.randomNumbers[curr];
     }, 0);
+    if (this.state.remainingSeconds === 0) {
+      return "LOST";
+    }
     if (sumSelected < this.target) {
       return "PLAYING";
     }
-    if (sumSelected === this.target) {
+    if (sumSelected === this.target && this.state.remainingSeconds > 0) {
       return "WON";
     }
     return "LOST";
   };
+
+  componentDidUpdate(prevProps: GameProps, prevState: GameState) {
+    const gameStatus = this.gameStatus();
+    if (gameStatus === "WON") {
+      setTimeout(this.resetGame, 1000); // Startet das Spiel nach einem Sieg neu, nach 1 Sekunde Verzögerung
+    }
+  }
 
   render() {
     const gameStatus = this.gameStatus();
@@ -61,13 +144,18 @@ class Game extends React.Component<GameProps, GameState> {
           </Text>
         </Text>
         <Text style={[statusStyle]}>Current status is: {gameStatus}</Text>
+        <Text style={[statusStyle]}>
+          Remaining Time is: {this.state.remainingSeconds}
+        </Text>
         <View style={styles.randomNumberContainer}>
           {this.randomNumbers.map((randomNumber, index) => (
             <Text style={styles.randomNumber} key={index}>
               <RandomNumber
                 number={randomNumber}
                 id={index}
-                isDisabled={this.isNumberSlected(index)}
+                isDisabled={
+                  this.isNumberSelected(index) || gameStatus !== "PLAYING"
+                }
                 onPress={() => this.selectNumber(index)}
               />
             </Text>
